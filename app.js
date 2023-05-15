4412796911222fa183e83d23a53569c306d59b75
Bartłomiej Skura <bartek33s@wp.pl>
2023-05-15 17:00:09 +0200

    Add first endpoint

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+import express from "express";
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.status(200).send("Hello world");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
